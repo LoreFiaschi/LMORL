c2dd9d2ee2f9a42209aba5fb3d008ec39c56ce27
Giacomo Pacini <giacomopacini98@gmail.com>
2023-05-11 11:48:03 +0200

    initial structure of LMORL library using gymnasium as basis

---
+++ LMORL/Environment.py
@@ -0,0 +1,18 @@
+from gymnasium import Env
+
+class Environment(Env):
+    """
+    this class extends MO Gymnasium Env class, it exposes the same methods to define a custom environment
+    - In addition it let specify NA weights for the multi objective reward vector
+    
+    """
+    def __init__(self) -> None:
+        pass
+
+    def get_scalar_reward(self):
+        """
+        The idea is that you can always get the scalarized reward.
+        1. In case of single objective reward, it is simply returned.
+        2. In case of MultiObjective reward array: it is obtained by multiplying the MO reward array by its weights (that can be Non Archimedean)).
+        """
+        pass
