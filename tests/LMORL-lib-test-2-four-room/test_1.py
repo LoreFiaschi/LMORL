8b061572e21ecd03570c2cf3cbcd77a68167eba0
Giacomo Pacini <giacomopacini98@gmail.com>
2023-05-17 18:25:26 +0200

    four rooms first test

---
+++ tests/LMORL-lib-test-2-four-room/test_1.py
@@ -0,0 +1,42 @@
+import sys
+import os
+from pathlib import Path
+
+root_dir = Path(os.getcwd())
+
+if str(root_dir.parents[1]) not in sys.path:
+  sys.path.append(str(root_dir.parents[1]))
+
+#print(sys.path)
+
+from LMORL.BAN.API.agents.DQNHybrid import DQNHybrid
+
+import mo_gymnasium as mo_gym
+
+
+env = mo_gym.make("four-room-v0", render_mode="rgb_array")
+
+input_size = env.observation_space.shape[0]
+num_actions = int(env.action_space.n)
+action_space = list(range(env.action_space.n))
+learning_rate = 0.001
+epsilon_decay = 0.996
+epsilon_min = 0.1
+batch_size = 64
+train_start = 64
+hidden_size = 128
+BAN_SIZE = 3
+max_memory_size = 100
+
+agent = DQNHybrid(input_size=input_size, num_actions=num_actions,
+                  action_space=action_space, learning_rate=learning_rate,
+                  epsilon_decay=epsilon_decay, epsilon_min=epsilon_min,
+                  batch_size=batch_size, hidden_size=hidden_size,
+                  ban_size=3, max_memory_size=max_memory_size, train_start=100)
+
+episodes = 350
+mname = "fooo.model"
+
+rewards, avg_rewards, timings = agent.agent_learning(env=env,episodes = episodes, mname=mname, render=True)
+
+
