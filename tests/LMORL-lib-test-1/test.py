5a20f12ceafe92e3f04a559fb0dbf2049c6d669c
Giacomo Pacini <giacomopacini98@gmail.com>
2023-05-17 18:25:58 +0200

    parametric memory size

--- tests/LMORL-lib-test-1/test.py
+++ tests/LMORL-lib-test-1/test.py
@@ -25,12 +25,13 @@ batch_size = 64
 train_start = 64
 hidden_size = 128
 BAN_SIZE = 3
+max_memory_size=100
 
 agent = DQNHybrid(input_size=input_size, num_actions=num_actions,
                   action_space=action_space, learning_rate=learning_rate,
                   epsilon_decay=epsilon_decay, epsilon_min=epsilon_min,
                   batch_size=batch_size, hidden_size=hidden_size,
-                  ban_size=3, max_memory_size=100, train_start=100)
+                  ban_size=3, max_memory_size=max_memory_size, train_start=100)
 
 episodes = 350
 mname = "fooo.model"
