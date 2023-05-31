5884c4d1e5a3ce3c064f7e5b9e2c17fe718e7e2d
Giacomo Pacini <giacomopacini98@gmail.com>
2023-05-31 13:10:09 +0200

    THRESHOLD_EXCEEDED_CONSECUTIVELY is now parametric

--- LMORL/BAN/API/agents/agent.py
+++ LMORL/BAN/API/agents/agent.py
@@ -74,13 +74,13 @@ class Agent(ABC):
             plt.pause(0.01)
         return img_frame
 
-    def learning(self, env : Environment, episodes : int, mname : str, replay_frequency = None, dump_period = None, early_stopping : Callable[[list], bool] = None, render : bool = False, verbose:bool=True):
+    def learning(self, env : Environment, episodes : int, mname : str, replay_frequency = None, dump_period = None, early_stopping : Callable[[list], bool] = None, THRESHOLD_EXCEEDED_CONSECUTIVELY = 3, render : bool = False, verbose:bool=True):
         """
         - if replay_frequency is None or is <= 0, then the learning is done at episode end
         - if replay_frequency is > 0, the learning is performed every replay_frequency timesteps
         - if dump_period is None no dump is performed, else it is performed every dump_period episodes
         returns rewards, avg_rewards, timings, infos
-        - early_stopping must be either None or a callable that returns True if the current average MO reward satisfies some type of requirement (environment dependent)
+        - early_stopping must be either None or a callable that returns True if the MO total reward of the current episode satisfies some type of requirement (environment dependent)
         - if early_stopping returns True a defined number of consecutive times, the learning is stopped
         - full_infos_dict is a dict of lists of lists: each element of the dictionary is a key of the env infos, and each of its lists is the list of infos of each timestep of an episode
         """
@@ -96,7 +96,7 @@ class Agent(ABC):
 
         solved = False
         threshold_exceeded = 0
-        THRESHOLD_EXCEEDED_CONSECUTIVELY = 3
+        
 
         episode_number = 1
 
@@ -166,7 +166,7 @@ class Agent(ABC):
             if verbose: print(f"{print_time}\tEpisode\t{episode_number}\ttimesteps:\t{t}\tTook\t{elapsed_episode} sec - reward:\t{totrew}\t| 100AvgReward: {avg_reward}")
             
             #if reward_threshold is not None and reward[0] >= reward_threshold:
-            if callable(early_stopping) and early_stopping(avg_reward) == True:
+            if callable(early_stopping) and early_stopping(totrew) == True:
                 # use as reward threshold the first component of the reward vector
                 threshold_exceeded += 1
                 if threshold_exceeded >= THRESHOLD_EXCEEDED_CONSECUTIVELY:
